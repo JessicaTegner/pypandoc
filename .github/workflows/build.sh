1c1733ed635a3485068a76cccd051a85e59f7d0f
JessicaTegner <jessica.tegner@outlook.com>
2022-10-27 17:47:15 +0200

    trying running docker manually

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -4,6 +4,6 @@ set -e -u -x
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
     "${PYBIN}/pip" install -U pip wheel setuptools twine
-    "${PYBIN}/python" setup_binary.py download_pandoc bdist_wheel
-    "${PYBIN}/python" setup.py sdist bdist_wheel
+    "${PYBIN}/python" /io/setup_binary.py download_pandoc bdist_wheel
+    "${PYBIN}/python" /io/setup.py sdist bdist_wheel
 done
