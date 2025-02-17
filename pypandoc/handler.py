f92dbb2833764f04e06f1811eb158db5ab7d8974
Kurt McKee <contactme@kurtmckee.org>
2025-02-17 11:39:14 -0600

    Use pre-commit (#396)
    * Add a basic pre-commit file
    
    * Run `pre-commit run -a`
    
    * Add pyupgrade as a pre-commit hook
    
    * Run `pre-commit run -a`
    
    * Add black as a pre-commit hook
    
    * Run `pre-commit run -a`
    
    * Add isort as a pre-commit hook
    
    * Run `pre-commit run -a`
    
    * Add flake8 as a pre-commit hook
    
    * Manually fix all flake8 errors
    
    * Add actionlint as a pre-commit hook
    
    * Manually fix errors reported by actionlint
    
    * Add Kurt McKee to the list of contributors

--- pypandoc/handler.py
+++ pypandoc/handler.py
@@ -1,24 +1,23 @@
-
 import logging
 
 logger = logging.getLogger(__name__.split(".")[0])
 
 
-
 def _check_log_handler():
-    
+
     # If logger has a handler do nothing
-    if logger.handlers: return
-    
+    if logger.handlers:
+        return
+
     # create console handler and set level to debug
     ch = logging.StreamHandler()
     ch.setLevel(logging.DEBUG)
-    
+
     # create formatter
-    formatter = logging.Formatter('[%(levelname)s] %(message)s')
-    
+    formatter = logging.Formatter("[%(levelname)s] %(message)s")
+
     # add formatter to ch
     ch.setFormatter(formatter)
-    
+
     # add ch to logger
     logger.addHandler(ch)
--- setup.py
+++ setup.py
@@ -1,30 +1,30 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-import pypandoc
-import io
+
 from setuptools import setup
 
-with io.open('README.md', encoding="utf-8") as f:
+import pypandoc
+
+with open("README.md", encoding="utf-8") as f:
     long_description = f.read()
 
 module = pypandoc
 setup(
-    name = 'pypandoc',
-    version = module.__version__,
-    url = pypandoc.__url__,
-    license = pypandoc.__license__,
-    description = pypandoc.__description__,
-    long_description = long_description,
-    long_description_content_type='text/markdown',
-    author = module.__author__.encode('utf8'),
-    author_email = pypandoc.__author_email__,
-    packages = ['pypandoc'],
+    name="pypandoc",
+    version=module.__version__,
+    url=pypandoc.__url__,
+    license=pypandoc.__license__,
+    description=pypandoc.__description__,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author=module.__author__.encode("utf8"),
+    author_email=pypandoc.__author_email__,
+    packages=["pypandoc"],
     python_requires=pypandoc.__python_requires__,
-    setup_requires = pypandoc.__setup_requires__,
+    setup_requires=pypandoc.__setup_requires__,
     classifiers=pypandoc.__classifiers__,
-    test_suite = 'tests',
+    test_suite="tests",
     project_urls={
-        'Source': pypandoc.__url__,
-        'Tracker': pypandoc.__url__ + '/issues',
-    }
+        "Source": pypandoc.__url__,
+        "Tracker": pypandoc.__url__ + "/issues",
+    },
 )
